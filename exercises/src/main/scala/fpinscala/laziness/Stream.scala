437624b79ba44cfa3f69badb62793eac3f10af98
Adam Coales <coalez@googlemail.com>
2017-12-07 21:54:34 +0000

    Complete Gen and fix compilation issues in previous chapters

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -1,35 +1,37 @@
 package fpinscala.laziness
 
-import Stream._
-import com.sun.crypto.provider.AESCipher.AES128_CBC_NoPadding
+import fpinscala.laziness.Stream._
 
 import scala.collection.mutable.ListBuffer
+
 trait Stream[+A] {
 
-    def toList: List[A] = this match {
+  def toList: List[A] = this match {
     case Empty => Nil
     case Cons(h, t) => h() :: t().toList
   }
 
   def toListFast: List[A] = {
     val buf = new ListBuffer[A]
+
     @annotation.tailrec
     def go(s: Stream[A]): List[A] = s match {
-      case Cons(h,t) =>
+      case Cons(h, t) =>
         buf += h()
         go(t())
       case _ => buf.toList
     }
+
     go(this)
   }
 
   def foldRight[B](z: => B)(f: (A, => B) => B): B = // The arrow `=>` in front of the argument type `B` means that the function `f` takes its second argument by name and may choose not to evaluate it.
     this match {
-      case Cons(h,t) => f(h(), t().foldRight(z)(f)) // If `f` doesn't evaluate its second argument, the recursion never occurs.
+      case Cons(h, t) => f(h(), t().foldRight(z)(f)) // If `f` doesn't evaluate its second argument, the recursion never occurs.
       case _ => z
     }
 
-  def exists(p: A => Boolean): Boolean = 
+  def exists(p: A => Boolean): Boolean =
     foldRight(false)((a, b) => p(a) || b) // Here `b` is the unevaluated recursive step that folds the tail of the stream. If `p(a)` returns `true`, `b` will never be evaluated and the computation terminates early.
 
   @annotation.tailrec
@@ -37,14 +39,15 @@ trait Stream[+A] {
     case Empty => None
     case Cons(h, t) => if (f(h())) Some(h()) else t().find(f)
   }
+
   def take(n: Int): Stream[A] = this match {
-    case Cons(h, t) if (n > 0) => cons(h(), t().take(n-1))
+    case Cons(h, t) if (n > 0) => cons(h(), t().take(n - 1))
     case _ => empty
   }
 
   def drop(n: Int): Stream[A] = this match {
-    case Cons(h, t) if (n > 0) => t().drop(n-1)
-    case _ => _
+    case Cons(h, t) if (n > 0) => t().drop(n - 1)
+    case x => x
   }
 
   def takeWhile(p: A => Boolean): Stream[A] = this match {
@@ -58,9 +61,9 @@ trait Stream[+A] {
   }
 
   def takeWhileFR(p: A => Boolean): Stream[A] =
-    this.foldRight(empty[A])((h, t) => if (p(h)) cons(h,t) else empty)
+    this.foldRight(empty[A])((h, t) => if (p(h)) cons(h, t) else empty)
 
-  def headOption: Option[A] = this.foldRight(None:Option[A])((a, _) => Some(a))
+  def headOption: Option[A] = this.foldRight(None: Option[A])((a, _) => Some(a))
 
   // 5.7 map, filter, append, flatmap using foldRight. Part of the exercise is
   // writing your own function signatures.
@@ -69,22 +72,22 @@ trait Stream[+A] {
 
   def filter(f: A => Boolean): Stream[A] = this.foldRight(empty[A])((h, t) => if (f(h)) cons(h, t) else t)
 
-  def append[B>:A](s2: => Stream[B]): Stream[B] = this.foldRight(s2)(cons(_, _))
+  def append[B >: A](s2: => Stream[B]): Stream[B] = this.foldRight(s2)(cons(_, _))
 
   def flatMap[B](f: A => Stream[B]): Stream[B] = this.foldRight(empty[B])((a, b) => f(a).append(b))
 
-  def mapUnfold[B](f: A => B): Stream[B] = unfold(this){
-    case Cons(h, t) => Some(f(h), t())
+  def mapUnfold[B](f: A => B): Stream[B] = unfold(this) {
+    case Cons(h, t) => Some((f(h()), t()))
     case _ => None
   }
 
-  def takeUnfold(n: Int): Stream[A] = unfold(this){
+  def takeUnfold(n: Int): Stream[A] = unfold(this) {
     case Cons(h, t) if (n > 0) => Some(h(), t())
     case _ => None
   }
 
   def takeWhileUnfold(p: A => Boolean): Stream[A] = unfold(this) {
-    case Cons(h, t) if (p(h)) => Some(h(), t())
+    case Cons(h, t) if (p(h())) => Some(h(), t())
     case _ => None
   }
 
@@ -118,7 +121,9 @@ trait Stream[+A] {
       (fp, Stream(fp).append(p._2))
     })._2
 }
+
 case object Empty extends Stream[Nothing]
+
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
 
 object Stream {
@@ -131,17 +136,18 @@ object Stream {
   def empty[A]: Stream[A] = Empty
 
   def apply[A](as: A*): Stream[A] =
-    if (as.isEmpty) empty 
+    if (as.isEmpty) empty
     else cons(as.head, apply(as.tail: _*))
 
   val ones: Stream[Int] = Stream.cons(1, ones)
 
   def constant[A](a: A): Stream[A] = cons(a, constant(a))
 
-  def from(n: Int): Stream[Int] = cons(n, from(n+1))
+  def from(n: Int): Stream[Int] = cons(n, from(n + 1))
 
   def fibs: Stream[Int] = {
-    def go(a: Int, b: Int): Stream[Int] = cons(a, go(b, a+b))
+    def go(a: Int, b: Int): Stream[Int] = cons(a, go(b, a + b))
+
     go(0, 1)
   }
 
@@ -155,7 +161,7 @@ object Stream {
 
   def constantUnfold[A](a: A): Stream[A] = unfold(a)(x => Some(x, x))
 
-  def fromUnfold(n: Int): Stream[Int] = unfold(n)(s => Some(s, s+1))
+  def fromUnfold(n: Int): Stream[Int] = unfold(n)(s => Some(s, s + 1))
 
   // could use match (s1, s2) => Some(s1, (s2, s1+s2))
   def fibsUnfold: Stream[Int] = unfold((0, 1))(s => Some(s._1, (s._2, s._1 + s._2)))
--- exercises/src/main/scala/fpinscala/testing/Gen.scala
+++ exercises/src/main/scala/fpinscala/testing/Gen.scala
@@ -1,33 +1,118 @@
 package fpinscala.testing
 
 import fpinscala.state.{RNG, State}
-import fpinscala.testing.Prop.{FailedCase, SuccessCount}
+import fpinscala.testing.Prop._
 
 /*
 The library developed in this chapter goes through several iterations. This file is just the
 shell, which you can fill in and modify while working through the chapter.
 */
 
-trait Prop {
-  def check: Either[(FailedCase, SuccessCount), SuccessCount]
+case class Prop(run: (MaxSize, TestCases, RNG) => Result) {
+  def &&(p: Prop): Prop = Prop {
+    (max, n, rng) => {
+      this.run(max, n, rng) match {
+        case Passed => p.run(max, n, rng)
+        case f => f
+      }
+    }
+  }
 
-  def &&(p: Prop): Prop = new Prop {
-    override def check: Boolean = Prop.this.check && p.check
+  def ||(p: Prop): Prop = Prop {
+    (max, n, rng) => {
+      this.run(max, n, rng) match {
+        case Falsified(_, _) => p.run(max, n, rng)
+        case p => p
+      }
+    }
   }
 }
 
 object Prop {
-  type FailedCase = String
   type SuccessCount = Int
+  type TestCases = Int
+  type MaxSize = Int
+  type FailedCase = String
 
-  def forAll[A](gen: Gen[A])(f: A => Boolean): Prop = ???
+  sealed trait Result {
+    def isFalsified: Boolean
+  }
+
+  case object Passed extends Result {
+    def isFalsified = false
+  }
+
+  case class Falsified(failure: FailedCase,
+                       successes: SuccessCount) extends Result {
+    def isFalsified = true
+  }
+
+  case object Proved extends Result {
+    def isFalsified = false
+  }
+
+  def randomStream[A](g: Gen[A])(rng: RNG): Stream[A] =
+    unfold(rng)(rng => Some(g.sample.run(rng)))
+
+  def forAll[A](as: Gen[A])(f: A => Boolean): Prop = Prop {
+    (max, n,rng) => randomStream(as)(rng).zip(Stream.from(0)).take(n).map {
+      case (a, i) => try {
+        if (f(a)) Passed else Falsified(a.toString, i)
+      } catch { case e: Exception => Falsified(buildMsg(a, e), i) }
+    }.find(_.isFalsified).getOrElse(Passed)
+  }
+
+  def unfold[A, B](start: B)(f: B => Option[(A, B)]): Stream[A] = f(start) match {
+    case Some((elem, next)) => elem #:: unfold(next)(f)
+    case None => Stream.empty
+  }
+
+  def check(p: => Boolean): Prop = Prop { (_, _, _) =>
+    if (p) Passed else Falsified("()", 0)
+  }
+
+  def buildMsg[A](s: A, e: Exception): String =
+    s"test case: $s\n" +
+      s"generated an exception: ${e.getMessage}\n" +
+      s"stack trace:\n ${e.getStackTrace.mkString("\n")}"
+
+  def forAll[A](g: SGen[A])(f: A => Boolean): Prop =
+    forAll(g.forSize)(f)
+
+  def forAll[A](g: Int => Gen[A])(f: A => Boolean): Prop = Prop {
+    (max,n,rng) =>
+      val casesPerSize = (n - 1) / max + 1
+      val props: Stream[Prop] =
+        Stream.from(0).take((n min max) + 1).map(i => forAll(g(i))(f))
+      val prop: Prop =
+        props.map(p => Prop { (max, n, rng) =>
+          p.run(max, casesPerSize, rng)
+        }).toList.reduce(_ && _)
+      prop.run(max,n,rng)
+  }
+
+  def run(p: Prop,
+          maxSize: Int = 100,
+          testCases: Int = 100,
+          rng: RNG = RNG.Simple(System.currentTimeMillis)): Unit =
+    p.run(maxSize, testCases, rng) match {
+      case Falsified(msg, n) =>
+        println(s"! Falsified after $n passed tests:\n $msg")
+      case Passed =>
+        println(s"+ OK, passed $testCases tests.")
+      case Proved =>
+        println(s"+ OK, proved property.")
+    }
+
 }
 
-case class Gen[A](sample: State[RNG, A]) {
+case class Gen[+A](sample: State[RNG, A]) {
 
-  def flatMap[A, B](f: A => Gen[B]): Gen[B] = Gen(sample.flatMap(a => f(a).sample))
+  def flatMap[B](f: A => Gen[B]): Gen[B] = Gen(sample.flatMap(a => f(a).sample))
 
-  def listOfN(size: Gen[Int]): Gen[List[A]] = size.flatMap(n => Gen.listOfN(n, this))
+  def listOfN(size: Gen[Int]): Gen[List[A]] = size.flatMap(Gen.listOfN(_, this))
+
+  def unsized: SGen[A] = SGen(_ => this)
 }
 
 object Gen {
@@ -39,7 +124,7 @@ object Gen {
 
   def choose(start: Int, stopExclusive: Int): Gen[Int] = Gen(State(RNG.nonNegativeInt).map(n => start + n % (stopExclusive - start)))
 
-  def union[A](g1: Gen[A], g2: Gen[A]): Gen[A] = boolean.flatMap(b => if (b) g1 else g2)
+  def union[A](g1: Gen[A], g2: Gen[A]): Gen[A] = boolean.flatMap((b: Boolean) => if (b) g1 else g2)
 
   def choose(max: Double): Gen[Double] = Gen(State(RNG.double).map(d => d % max))
 
@@ -49,13 +134,34 @@ object Gen {
   }))
 }
 
-trait Gen[A] {
-  def map[A, B](f: A => B): Gen[B] = ???
-
-  def flatMap[A, B](f: A => Gen[B]): Gen[B] = ???
+case class SGen[+A](forSize: Int => Gen[A]) {
+  def flatMap[B](f: A => SGen[B]): SGen[B] = SGen {
+    n => forSize(n).flatMap(f(_).forSize(n))
+  }
 }
 
-trait SGen[+A] {
+object SGen {
+  def listOf[A](g: Gen[A]): SGen[List[A]] = SGen(n => Gen.listOfN(n, g))
 
+  def listOf1[A](g: Gen[A]): SGen[List[A]] = SGen(n => Gen.listOfN(n.max(1), g))
 }
 
+object Tester {
+
+  def main(args: Array[String]): Unit = {
+    val smallInt = Gen.choose(-10, 10)
+    val maxProp = Prop.forAll(SGen.listOf1(smallInt)) { ns =>
+      val max = ns.max
+      !ns.exists(_ > max)
+    }
+    Prop.run(maxProp)
+
+    val sortedProp = Prop.forAll(SGen.listOf1(smallInt)) { ns =>
+      val sorted = ns.sorted
+      !sorted.exists(!ns.contains(_)) &&
+      !ns.exists(!sorted.contains(_)) &&
+      sorted.foldRight((Integer.MAX_VALUE, true))((x, y) => (x, x <= y._1 && y._2))._2
+    }
+    Prop.run(sortedProp)
+  }
+}
